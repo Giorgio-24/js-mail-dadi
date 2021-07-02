974544537ebe42b0b95211b2f622340fa8195fd1
Giorgio Andrei <cipuandrei@libero.it>
2021-07-02 16:01:26 +0200

    files-connected

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
+
+var userMail = prompt('Inserisci qui la tua email: ')
