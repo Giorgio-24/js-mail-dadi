a1f09c5f343e398d125e62aa5e23c73d585e1c7b
Giorgio Andrei <cipuandrei@libero.it>
2021-07-02 18:15:14 +0200

    email-verification-done

--- script.js
+++ script.js
@@ -3,4 +3,38 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
-var userMail = prompt('Inserisci qui la tua email: ')
+//= VARIABLES
+var message = 'Lei è autorizzato a procedere, benvenuto signore.'
+
+var unauthorisedMessage = 'ATTENZIONE!! Non sei autorizzato a procedere.'
+
+var userMail = prompt('Inserisci qui la tua email: ');
+console.log(userMail)
+
+var authorisedUsers = ['nietzschefriedrich@gmail.com', 'sigmundfreud@gmail.com', 'marxkarl@gmail.com', 'immanuelkant@gmail.com', 'arthurschopenhauer@gmail.com'];
+console.log(authorisedUsers)
+
+var correspondance = false;
+
+var lenght = authorisedUsers.length;
+
+
+//= CICLE
+for (var i = 0; 1 < length; i++) {
+  if (userMail == authorisedUsers[i]){
+      correspondance = true;
+  }
+}
+
+if (correspondance = true){
+    var result = document.getElementById('typed-email');
+    console.log(result)
+
+    result.innerHTML = message;
+} else {
+    var result = document.getElementById('typed-email');
+    console.log(result)
+
+    result.innerHTML = unauthorisedMessage;
+}
+
