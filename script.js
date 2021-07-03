0ebfbf7141839f95cf1cd1a915a2ab3c7da01d59
Giorgio Andrei <cipuandrei@libero.it>
2021-07-03 09:56:01 +0200

    dice-games-done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
-//= VARIABLES
+/* //= VARIABLES
 var message = 'Lei è autorizzato a procedere, benvenuto signore.'
 
 var unauthorisedMessage = 'ATTENZIONE!! Non sei autorizzato a procedere.'
@@ -16,25 +16,48 @@ console.log(authorisedUsers)
 
 var correspondance = false;
 
-var lenght = authorisedUsers.length;
+var length = authorisedUsers.length;
 
 
 //= CICLE
 for (var i = 0; 1 < length; i++) {
-  if (userMail == authorisedUsers[i]){
+  if (userMail == authorisedUsers[i]) {
       correspondance = true;
   }
 }
 
-if (correspondance = true){
-    var result = document.getElementById('typed-email');
-    console.log(result)
+if (correspondance) {
+    var result = document.getElementById('authorised');
+    console.log(result);
 
     result.innerHTML = message;
 } else {
-    var result = document.getElementById('typed-email');
-    console.log(result)
+    var result = document.getElementById('unauthorised');
+    console.log(result);
 
     result.innerHTML = unauthorisedMessage;
-}
+}  //! VA ALL'INFINITO */
 
+
+//^GIOCO DEI DADI
+
+//= VARIABLES
+
+var x = 'VAI!';
+
+prompt('Tira il tuo dado.', x)
+
+var userNumber = Math.floor(Math.random() * 6) + 1;
+console.log(userNumber)
+
+var cpuNumber = Math.floor(Math.random() * 6) + 1;
+console.log(cpuNumber)
+
+//=CONDITIONS
+if (userNumber > cpuNumber){
+    alert('CONGRATULAZIONI! Hai vinto.')
+} else if (userNumber < cpuNumber){
+    alert('OH NO! Hai perso, che sfortuna.')
+} else {
+    alert('PAREGGIO! Gioca di nuovo la partita.')
+}
