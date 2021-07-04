9ccf3628f8e0b76585bcfff13810de397f3ea538
Giorgio Andrei <cipuandrei@libero.it>
2021-07-04 20:28:24 +0200

    general-adjustments-and-style-added

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ for (var i = 0; i < len; i++) {
 
 if (bool) {
     var result = document.getElementById('validation')
-    result.innerHtml = 'Lei è autorizzato a procedere, bentornato signore.';
+    result.style.backgroundColor = "#00ff00"
+    result.innerHTML = 'Lei è autorizzato a procedere, bentornato signore.';
     console.log(result);
 } else {
     var result = document.getElementById('validation')
+    result.style.backgroundColor = "#ff0000"
     result.innerHTML = 'ATTENZIONE!! Non sei autorizzato a procedere.';
     console.log(result);
 }
@@ -38,44 +40,47 @@ var yourNumberIs = 'Dal tuo dado è uscito il numero: '
 var enemyNumberIs = 'Dal dado del tuo avversario è uscito il numero: '
 
 //=CICLE
-        for (var y = 0; y < 1; y++){
+for (var y = 0; y < 1; y++){
 
-        var x = 'VAI!';
+  var x = 'VAI!';
 
-        prompt('Tira il tuo dado.', x)
+  prompt('Tira il tuo dado.', x)
 
-        var userNumber = Math.floor(Math.random() * 6) + 1;
-        console.log(userNumber)
+  var userNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(userNumber)
         
-        var cpuNumber = Math.floor(Math.random() * 6) + 1;
-        console.log(cpuNumber)
+  var cpuNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(cpuNumber)
 
-        if (userNumber > cpuNumber){
-            var yourResult = document.getElementById('usernumber')
-            yourResult.innerText = yourNumberIs + userNumber;
-            console.log(yourResult)
+ if (userNumber > cpuNumber){
+     var yourResult = document.getElementById('usernumber')
+     yourResult.innerText = yourNumberIs + userNumber;
+     console.log(yourResult)
         
-            var enemyResult = document.getElementById('cpunumber')
-            enemyResult.innerText = enemyNumberIs + cpuNumber;
-            console.log(enemyResult)
+     var enemyResult = document.getElementById('cpunumber')
+     enemyResult.innerText = enemyNumberIs + cpuNumber;
+     console.log(enemyResult)
         
-            var finalDiceResult = document.getElementById('result')
-            finalDiceResult.innerText = winningMessage;
-            console.log(finalDiceResult.innerText)
-        } else if (userNumber < cpuNumber){
-            var yourResult = document.getElementById('usernumber')
-            yourResult.innerText = yourNumberIs + userNumber;
-            console.log(yourResult)
+     var finalDiceResult = document.getElementById('result')
+     finalDiceResult.innerText = winningMessage;
+     finalDiceResult.style.backgroundColor = "#00ff00"
+     console.log(finalDiceResult.innerText)
+ } else if (userNumber < cpuNumber){
+     var yourResult = document.getElementById('usernumber')
+     yourResult.innerText = yourNumberIs + userNumber;
+     console.log(yourResult)
         
-            var enemyResult = document.getElementById('cpunumber')
-            enemyResult.innerText = enemyNumberIs + cpuNumber;
-            console.log(enemyResult)
+     var enemyResult = document.getElementById('cpunumber')
+     enemyResult.innerText = enemyNumberIs + cpuNumber;
+     console.log(enemyResult)
         
-            var finalDiceResult = document.getElementById('result')
-            finalDiceResult.innerText = losingMessage;
-            console.log(finalDiceResult.innerText)
-        } else {
-            var y = --y;
-            alert('PAREGGIO! Gioca di nuovo la partita.')
-        }
-    }
+     var finalDiceResult = document.getElementById('result')
+     finalDiceResult.innerText = losingMessage;
+     finalDiceResult.style.backgroundColor = "#ff0000"
+     console.log(finalDiceResult.innerText)
+ } else {
+     var y = --y;
+     alert('PAREGGIO! Gioca di nuovo la partita.')
+ } 
+
+}
